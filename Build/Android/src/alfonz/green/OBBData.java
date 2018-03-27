e808790c0dce7ee7587afe9f8f38346cb689ac33
mendiolanivelle3123 <mendiolanivelle@gmail.com>
2018-03-27 21:47:21 +0800

    ttatatatatatata

--- Build/Android/src/alfonz/green/OBBData.java
+++ Build/Android/src/alfonz/green/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-33782546L // the length of the file in bytes
+48213823L // the length of the file in bytes
 )
 };
 };
